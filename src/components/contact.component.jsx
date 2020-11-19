be6d8efbc4306957434d377e840a386cba29e53d
csimbulan <c.simbulan@hotmail.com>
2020-11-19 18:44:24 -0500

    test submit

--- src/components/contact.component.jsx
+++ src/components/contact.component.jsx
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import { MDBCol, MDBBtn } from "mdbreact";
 
-function encode(data) {
-    return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
-}
-
 class Contact extends Component {
 
     state = {
