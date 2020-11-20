9b0e35072c26d6ff3cac206a6e6473c6eb639ff8
csimbulan <c.simbulan@hotmail.com>
2020-11-19 20:44:57 -0500

    fixed grammar in about

--- src/components/about.component.jsx
+++ src/components/about.component.jsx
@@ -11,8 +11,10 @@ const About = () => {
                     </div>
                     <div className="container-custom banner-body max-width">
                         <span className="span-text">
-                            <span className="helloValentica">Hello, my name is Chris.</span><br />Welcome to my site! I am self taught programmer looking for opportunities to push my boundaries and grow alongside a team.
-                            I'm always excited to learn new technologies. Check out some of my projects below!
+                            <span className="helloValentica">Hello, my name is Chris.</span><br />
+                            Welcome to my site! I am a self-taught programmer looking for opportunities
+                            to push my boundaries and grow alongside a team. I'm always excited to learn
+                            new technologies. Check out some of my projects below!
                     </span>
                     </div>
                 </div>
