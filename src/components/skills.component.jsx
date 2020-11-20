539b818904bc52de060aa20a82729ea222e75c83
csimbulan <c.simbulan@hotmail.com>
2020-11-19 21:41:02 -0500

    fixed skills section responsiveness

--- src/components/skills.component.jsx
+++ src/components/skills.component.jsx
@@ -5,11 +5,11 @@ const Skills = () => {
     return (
         <div className="container-custom full-vw-width skills mini-section">
             <MDBCol className="container-custom small-padding mobile-spacer" lg="4">
-                <div className="white-shadow">
-                    <div className="container-custom gradient-blue banner-header max-width">
+                <div className="white-shadow max-width">
+                    <div className="container-custom banner-header gradient-blue max-width">
                         <h2 className="white-text">Front End</h2>
                     </div>
-                    <div className="icons-container banner-body max-width tech-min-height ">
+                    <div className="icons-container banner-body max-width tech-min-height">
                         <div className="icon-container">
                             <svg viewBox="0 0 128 128">
                                 <path fill="#E44D26" d="M19.037 113.876l-10.005-112.215h109.936l-10.016 112.198-45.019 12.48z"></path><path fill="#F16529" d="M64 116.8l36.378-10.086 8.559-95.878h-44.937z"></path><path fill="#EBEBEB" d="M64 52.455h-18.212l-1.258-14.094h19.47v-13.762h-34.511l.33 3.692 3.382 37.927h30.799zM64 88.198l-.061.017-15.327-4.14-.979-10.975h-13.817l1.928 21.609 28.193 7.826.063-.017z"></path><path fill="#fff" d="M63.952 52.455v13.763h16.947l-1.597 17.849-15.35 4.143v14.319l28.215-7.82.207-2.325 3.234-36.233.335-3.696h-3.708zM63.952 24.599v13.762h33.244l.276-3.092.628-6.978.329-3.692z"></path>
@@ -52,7 +52,7 @@ const Skills = () => {
             </MDBCol>
 
             <MDBCol className="container-custom max-width small-padding mobile-spacer" lg="4">
-                <div className="white-shadow">
+                <div className="white-shadow max-width">
                     <div className="container-custom banner-header gradient-blue max-width">
                         <h2 className="white-text">Back End</h2>
                     </div>
